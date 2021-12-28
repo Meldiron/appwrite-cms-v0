41cbd5fc01b32b78d5bea77e8bc2648875c82811
Matej Baco <matejbacocom@gmail.com>
2021-12-28 21:52:31 +0100

    SEO fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,10 @@
+const title = 'AppwriteCMS | Headless Admin Panel'
+const description =
+  'Prepare admin panels for your projects rapidly using Appwrite CMS, a headless CMS for any Appwrite project.'
+const cover =
+  'https://raw.githubusercontent.com/Meldiron/appwrite-cms/master/static/cover.png'
+const url = undefined
+
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
@@ -11,15 +18,45 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'AppwriteCMS',
+    title: title,
     htmlAttrs: {
       lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
+
+      { hid: 'og:title', name: 'og:title', content: title },
+      { hid: 'description', name: 'description', content: description },
+      // { hid: 'og:url', name: 'og:url', content: url },
+      { hid: 'og:type', name: 'og:type', content: 'website' },
+      { hid: 'og:description', name: 'og:description', description },
+      { hid: 'og:image', name: 'og:image', content: cover },
+      {
+        hid: 'twitter:card',
+        name: 'twitter:card',
+        content: 'summary_large_image',
+      },
+      {
+        hid: 'twitter:domain',
+        name: 'twitter:domain',
+        content: url
+          .split('http://')
+          .join('')
+          .split('https://')
+          .join('')
+          .split('/')
+          .join(''),
+      },
+      // { hid: 'twitter:url', name: 'twitter:url', content: url },
+      { hid: 'twitter:title', name: 'twitter:title', content: title },
+      {
+        hid: 'twitter:description',
+        name: 'twitter:description',
+        content: description,
+      },
+      { hid: 'twitter:image', name: 'twitter:image', content: cover },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
