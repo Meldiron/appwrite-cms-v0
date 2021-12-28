b36c67c8c1d527876b51a9a182cd147275fa601e
Matej Baco <matejbacocom@gmail.com>
2021-12-28 21:48:41 +0100

    Fixed SEO on default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,6 +4,15 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import { Utils } from '../services/utils'
+import { mapGetters } from 'vuex'
 
-export default Vue.extend({})
+export default Vue.extend({
+  computed: {
+    ...mapGetters('config', ['config', 'menuTree']),
+  },
+  head() {
+    return Utils.generateHead(this.config)
+  },
+})
 </script>
